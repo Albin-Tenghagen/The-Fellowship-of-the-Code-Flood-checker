e4c95de943a21d518588b0f1f4808dc227485044
AnnaSchwartzChas <anna.schwartz@chasacademy.se>
2025-05-05 15:55:51 +0200

    Changed some variables name after the code standard.

--- embedded-systems/include/SoilSensor.h
+++ embedded-systems/include/SoilSensor.h
@@ -4,56 +4,60 @@
 
 namespace Soil 
 {
-  uint16_t SoilSensor = A0;
-  uint8_t SoilSensorPower = 4;
-  uint16_t SoilReadingValue = 0;
-  uint8_t SoilReadingValueInPrecentage;
+  uint16_t soil_sensor = A0;
+  uint8_t soil_sensor_power = 4;
+  uint16_t soil_reading_value = 0;
+  uint8_t soil_reading_value_in_precentage;
 
-  unsigned long previousReading = 0;
-  unsigned long intervalReading = 10000;
+  // Delete these variables for the real program
+  unsigned long previous_reading = 0;
+  unsigned long interval_reading = 10000;
 
   // Function prototypes
   void readSoilMoistureSensor();
   void turnSoilMoistureToPrecentage();
-  void printSoilMoistureSensorValues();
+  void printSoilMoistureSensorValues(); //Comment out/delete this function for the real program
   void updateSoilSensorValue();
 
   void InitiateSoilSensor() 
   {
-    pinMode(SoilSensor,INPUT);
-    pinMode(SoilSensorPower,OUTPUT);
-    digitalWrite(SoilSensorPower,LOW); 
+    pinMode(soil_sensor,INPUT);
+    pinMode(soil_sensor_power,OUTPUT);
+    digitalWrite(soil_sensor_power,LOW); 
     Serial.begin(9600);
   }
 
   void updateSoilSensorValue() 
   {
-    if(millis() - previousReading >= intervalReading) 
+    /*Delete millis and if statement for the real program, just run the readSoilMoistureSensor function,
+    digitalWrite, turnSoilMoistureToPrecentage function. No need to run printSoilMoistureSensorValues */
+    if(millis() - previous_reading >= interval_reading) 
     {
-      previousReading = millis();
+      previous_reading = millis();
       readSoilMoistureSensor();
-      digitalWrite(SoilSensorPower,LOW);
+      digitalWrite(soil_sensor_power,LOW);
       turnSoilMoistureToPrecentage(); 
-      printSoilMoistureSensorValues();
+      printSoilMoistureSensorValues();//Comment out/delete this function for the real program
     }
   
   }
 
   void readSoilMoistureSensor()
   {
-    digitalWrite(SoilSensorPower,HIGH);
-    SoilReadingValue = analogRead(SoilSensor);
+    digitalWrite(soil_sensor_power,HIGH);
+    soil_reading_value = analogRead(soil_sensor);
   }
 
+  //Comment out/delete this function for the real program, just for checking that the sensor is working
   void printSoilMoistureSensorValues()
   {
-    Serial.println(SoilReadingValue);
-    Serial.println(SoilReadingValueInPrecentage);
+    Serial.println(soil_reading_value);
+    Serial.println(soil_reading_value_in_precentage);
   }
 
   void turnSoilMoistureToPrecentage()
   {
-    SoilReadingValueInPrecentage = map(SoilReadingValue, 270, 1023, 100, 0);
+    soil_reading_value_in_precentage = map(soil_reading_value, 270, 1023, 100, 0);
   }
 }
 #endif
