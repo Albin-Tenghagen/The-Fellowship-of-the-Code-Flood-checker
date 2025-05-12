901cd0be3088687145435ee63a38367ab946d0e2
AnnaSchwartzChas <anna.schwartz@chasacademy.se>
2025-05-12 15:46:35 +0200

    Deleted json changes. Will make a new branch for it.

--- embedded-systems/src/main.cpp
+++ embedded-systems/src/main.cpp
@@ -1,20 +1,12 @@
 #include <Arduino.h>
 #include "SoilSensor.h"
-#include <LittleFS.h>
-#include <ArduinoJson.h>
 
 //Delete these variables in the real program
 unsigned long previous_reading = 0;
 unsigned long interval_reading = 10000;
 
-const char *filename = "/SensorData.json";
-
 void setup() 
 {
-  if (!LittleFS.begin()) {
-    Serial.println("LittleFS kunde inte startas");
-    return;
-  }
   Soil::InitiateSoilSensor();
 }
 
@@ -26,30 +18,5 @@ void loop()
     {
       //Soil::updateSoilSensorValue();
       previous_reading = millis();
-    
-    // Skapa JSON-objekt
-    //StaticJsonDocument<200> jsonDoc;
-    //jsonDoc["SoilMoisture"] = Soil::soil_reading_value_in_precentage;
-
-  StaticJsonDocument<256> jsonDoc;
-  jsonDoc["SoilMoisture"] = 58;
-  jsonDoc["Temperature"] = 22.5;
-  jsonDoc["Humidity"] = 45;
-  jsonDoc["WaterlevelPressure"] = 105; 
-  jsonDoc["WaterlevelUltraSound"] = 105.5;
-
-
-
-    // Skriv till fil (överskriv)
-    File file = LittleFS.open(filename, "w");
-    if (!file) {
-      Serial.println("Kunde inte öppna filen för skrivning");
-      return;
     }
-
-    serializeJson(jsonDoc, file);
-    file.close();
-
-    Serial.println("Sensordata sparad till JSON-fil");
-  }
 }
