9258bf0f0c517ae9cbc7a98bf1b07ec965c9ad67
Love Lindeborg <lindeborglove@gmail.com>
2025-05-12 12:06:04 +0200

    Added test code

--- embedded-systems/src/main.cpp
+++ embedded-systems/src/main.cpp
@@ -4,6 +4,7 @@
 #include <WiFi.h>
 
 #include "lora/fellowship_lora.h"
+#include "wifi/fellowship_wifi.h"
 
 void connectToWiFi()
 {
@@ -40,47 +41,55 @@ void setup()
 
     while (!Serial);
 
-    connectToWiFi();
+    fellowshipWiFi::connectWiFi("HUAWEI-B535-0B48", "2MLRG8FFA58", { 192, 168, 8, 198 }, { 192, 168, 8, 1 }, { 255, 255, 255, 0 });
+    fellowshipWiFi::sendData(GET, {192, 168, 8, 169}, 5000, "/", "");
 
-    if (client.connect(IPAddress{192, 168, 8, 169}, 5000))
-    {
-        Serial.println("Connected to server");
-        client.println("GET / HTTP/1.1");
-        client.println("Host: 192.168.8.169");
-        client.println("Connection: close");
-        client.println();
-        // client.println("GET / HTTP/1.1");
-    }
-    else
-    {
-        Serial.println("Something went wrong");
-        Serial.println(WiFi.status());
-    }
+    String data;
+    fellowshipWiFi::recieveData(data);
 
-    Serial.println("Headers sent");
+    Serial.println(data);
 
-    Serial.println(client.available());
-    while(client.connected())
-    {
-    // read an incoming byte from the server and print them to serial monitor:
-        if (client.available())
-            Serial.print(client.readString());
-    }
+    // connectToWiFi();
 
-    if(!client.connected())
-    {
-    // if the server's disconnected, stop the client:
-        Serial.println("\nDisconnected");
-        client.stop();
-    }
+    // if (client.connect(IPAddress{192, 168, 8, 169}, 5000))
+    // {
+    //     Serial.println("Connected to server");
+    //     client.println("GET / HTTP/1.1");
+    //     client.println("Host: 192.168.8.169");
+    //     client.println("Connection: close");
+    //     client.println();
+    //     // client.println("GET / HTTP/1.1");
+    // }
+    // else
+    // {
+    //     Serial.println("Something went wrong");
+    //     Serial.println(WiFi.status());
+    // }
+
+    // Serial.println("Headers sent");
+
+    // Serial.println(client.available());
+    // while(client.connected())
+    // {
+    // // read an incoming byte from the server and print them to serial monitor:
+    //     if (client.available())
+    //         Serial.print(client.readString());
+    // }
+
+    // if(!client.connected())
+    // {
+    // // if the server's disconnected, stop the client:
+    //     Serial.println("\nDisconnected");
+    //     client.stop();
+    // }
 
     
 
     // FellowshipLoRa::init();
 
-    // if (!FellowshipLoRa::wasInit() || FellowshipLoRa::errorFlag != RADIOLIB_ERR_NONE) 
+    // if (!FellowshipLoRa::was_init() || FellowshipLoRa::error_flag != RADIOLIB_ERR_NONE) 
     // {
-    //     Serial.println(FellowshipLoRa::errorMsg);
+    //     Serial.println(FellowshipLoRa::error_msg);
     //     return;
     // }
 
