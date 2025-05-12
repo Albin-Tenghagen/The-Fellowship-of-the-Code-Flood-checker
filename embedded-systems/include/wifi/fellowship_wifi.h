7220865e41111472a8bcc2fba96099cef0e8abab
Love Lindeborg <lindeborglove@gmail.com>
2025-05-12 12:06:25 +0200

    Begin work on WiFi

---
+++ embedded-systems/include/wifi/fellowship_wifi.h
@@ -0,0 +1,21 @@
+#ifndef FELLOWSHIP_WIFI_H
+#define FELLOWSHIP_WIFI_H
+
+#include <WString.h>
+#include <WiFi.h>
+
+#include "fellowship_wifi_config.h"
+
+namespace fellowshipWiFi
+{
+    static WiFiClient client{};
+    
+    bool connectWiFi(const char *ssid, const char *passphrase, IPAddress local_ipaddr, IPAddress gateway, IPAddress subnet_mask);
+    bool disconnectWiFi();
+
+    bool sendData(HTTPMethod method, IPAddress host, uint32_t port, String endpoint, String data);
+    bool recieveData(String &buffer);
+} 
+
+
+#endif
