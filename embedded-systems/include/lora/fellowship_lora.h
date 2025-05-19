a2ae6cdfcd15b74a9e53bb0ba5eba4a3019d1727
Love Lindeborg <lindeborglove@gmail.com>
2025-05-19 09:50:20 +0200

    Added doxygen documentation

--- embedded-systems/include/lora/fellowship_lora.h
+++ embedded-systems/include/lora/fellowship_lora.h
@@ -1,3 +1,14 @@
+/**
+ * @file fellowship_lora.h
+ * @author Love Lindeborg (love.lindeborg@chasacademy.se)
+ * @brief This is the wrapper for RadioLib SX1262 LoRa module.
+ * @version 0.1
+ * @date 2025-05-19
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+*/
+
 #ifndef WIZARD_LORA
 #define WIZARD_LORA
 
@@ -7,24 +18,74 @@
 
 namespace fellowshipLoRa {
 
+    /**
+     * @brief This variable stores flags relevant to LoRa. 
+     * @warning Do not use.
+     */
     static struct LoRaFlags {
-        bool transmit_flag = false;
         bool was_init = false;
-        bool is_server = false;
     } flags;
 
+    /**
+     * @brief Stores error codes from all functions which are called from within this namespace. Is RADIOLIB_ERR.
+     * @warning Do not set, read only
+     */
     static int16_t error_flag = RADIOLIB_ERR_NONE;
+
+    /**
+     * @brief Stores a human readable version of the error code, however, it is more generic.
+     * @warning Do not set.
+     */
     static String error_msg;
 
+    /**
+     * @brief LoRa device, default pins and other configuration options is located in "fellowship_lora_config.h"
+     */
     static SX1262 *device;
 
+    /**
+     * @brief Initializes LoRa so it can be used by other functions. Uses `LORA_NSS`, `LORA_DIO1`, `LORA_RST`, and `LORA_BUSY` 
+     * @returns RADIOLIB_ERR (int16_t)
+     */
     int16_t init();
+
+    /**
+     * @brief Checks if LoRa was initialized.
+     * 
+     * @returns RADIOLIB_ERR (int16_t)
+     */
     int16_t was_init();
     
+    /**
+     * @brief Reads a message from LoRa. This function is blocking.
+     * 
+     * @param buffer String to write message to
+     * @returns RADIOLIB_ERR (int16_t). On timeout function returns `RADIOLIB_ERR_RX_TIMEOUT`
+     */
     int16_t read(String &buffer);
+
+    /**
+     * @brief Reads a message from LoRa but unlike `read(String&)` does not return a timeout. This function is blocking.
+     * 
+     * @param buffer String to write message to
+     * @returns RADIOLIB_ERR (int16_t). On timeout function returns `RADIOLIB_ERR_RX_TIMEOUT`
+     */
     int16_t readUntilValueRecv(String &buffer);
 
+    /**
+     * @brief Converts `value` to two 8-bit values and writes message to LoRa.
+     * 
+     * @param value Value to be converted to two 8-bit values.
+     * @returns RADIOLIB_ERR (int16_t).
+     */
     int16_t write(int16_t value);
+
+    /**
+     * @brief Transmits a string using LoRa. 
+     * 
+     * @param msg String to be transmitted.
+     * @returns RADIOLIB_ERR (int16_t).
+     */
     int16_t write(String &msg);
 };
 
--- embedded-systems/include/wifi/fellowship_wifi.h
+++ embedded-systems/include/wifi/fellowship_wifi.h
@@ -1,3 +1,14 @@
+/**
+ * @file fellowship_wifi.h
+ * @author Love Lindeborg (love.lindeborg@chasacademy.se)
+ * @brief Definitions for connecting to WiFi
+ * @version 0.1
+ * @date 2025-05-19
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
 #ifndef FELLOWSHIP_WIFI_H
 #define FELLOWSHIP_WIFI_H
 
@@ -10,10 +21,48 @@ namespace fellowshipWiFi
 {
     static WiFiClient client{};
     
-    bool connectWiFi(const char *ssid, const char *passphrase, IPAddress local_ipaddr, IPAddress gateway, IPAddress subnet_mask);
+    /**
+     * @brief Connects the device to the internet. Uses SSID and PASSPHRASE from secrets.h which is not included by git.
+     * 
+     * @param local_ipaddr Static IPv4 address in the local network
+     * @param gateway The networks gateway
+     * @param subnet_mask Which networkmask to be used
+     * @returns true if connection succeeded, false otherwise.
+     */
+    bool connectWiFi(IPAddress local_ipaddr, IPAddress gateway = {192, 168, 1, 1}, IPAddress subnet_mask = {255, 255, 255, 0});
+
+    /**
+     * @brief Connects the device to the internet using DHCP.  
+
+     * @returns true if connection succeeded, false otherwise.
+     */
+    bool connectWiFi();
+
+    /**
+     * @brief Disconnects from the internet
+     * 
+     * @returns true if succeeded
+     * @returns false if couldn't disconnect
+     */
     bool disconnectWiFi();
 
+    /**
+     * @brief Sends a POST request to `host` on `port`.
+     * 
+     * @param host Host to send data to
+     * @param port Port to send data to
+     * @param endpoint Server endpoint such as /api/endpoint.
+     * @param data Data to be sent, as JSON
+     * @returns true if succeeded to send data.
+     */
     bool sendRequest(IPAddress host, uint32_t port, String endpoint, String data);
+
+    /**
+     * @brief Wait for data to be recieved.
+     * 
+     * @param buffer 
+     * @returns true if succeeded 
+     */
     bool recieveData(String &buffer);
 } 
 
