38a3d362a2ea4223b3eb76615f3b3b91796b2ea7
AnnaSchwartzChas <anna.schwartz@chasacademy.se>
2025-05-19 11:52:37 +0200

    Started to add the other sensors files and run them all in main.cpp.

---
+++ embedded-systems/include/hcsr04.h
@@ -0,0 +1,25 @@
+#ifndef HCSR04_H
+#define HCSR04_H
+
+#include <Arduino.h>
+
+namespace hcsr04
+{
+    struct SensorConfig
+    {
+        uint8_t trig_pin;
+        uint8_t echo_pin;
+        float calibration_offset_cm;
+    };
+
+    extern SensorConfig config;
+
+    void begin(uint8_t trig_pin, uint8_t echo_pin);
+    void setMockMode(bool enable);
+    void setMockDuration(unsigned long duration_us);
+    float readDistance(); // returns distance in cm
+    void calibrateZero(float known_level_cm);
+    unsigned long simulateEchoDurationFromCM(float cm); // used only in mock mode
+}
+
+#endif
--- embedded-systems/src/main.cpp
+++ embedded-systems/src/main.cpp
@@ -1,13 +1,19 @@
 #include <Arduino.h>
 #include "SoilSensor.h"
+#include "hcsr04.h"
+#include <DHTSensor.h>
 
 //Delete these variables in the real program
 unsigned long previous_reading = 0;
 unsigned long interval_reading = 10000;
+float HCSR04distance = 0.0;
 
 void setup() 
 {
   Soil::InitiateSoilSensor();
+  DHTSensor::InitDHTSensor();
+  hcsr04::begin(20, 19);
+  hcsr04::calibrateZero(20.0);
 }
 
 void loop() 
@@ -17,6 +23,12 @@ void loop()
   if(millis() - previous_reading >= interval_reading) 
     {
       Soil::updateSoilSensorValue();
+      HCSR04distance = hcsr04::readDistance();
+      Serial.print("Distance: ");
+      Serial.print(HCSR04distance);
+      Serial.println(" cm");
+      DHTSensor::ReadDHTSensor();
+      DHTSensor::PrintDHTSensor();
       previous_reading = millis();
     }
 }
