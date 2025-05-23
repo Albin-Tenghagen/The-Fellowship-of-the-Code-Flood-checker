6ad68df3d3bfa95845ccd69635937c5f36a8b5b6
Love Lindeborg <lindeborglove@gmail.com>
2025-05-23 14:57:13 +0200

    Slight modifications to code for easier change of pins

--- embedded-systems/include/SoilSensor.h
+++ embedded-systems/include/SoilSensor.h
@@ -4,10 +4,10 @@
 
 namespace Soil 
 {
-  uint16_t soil_sensor = 7;
-  uint8_t soil_sensor_power = 6;
+  uint16_t soil_sensor_pin = 7;
+  uint16_t soil_sensor_power_pin = 6;
   uint16_t soil_reading_value = 0;
-  uint8_t soil_reading_value_in_precentage;
+  uint16_t soil_reading_value_in_precentage;
 
   // Function prototypes
   void readSoilMoistureSensor();
@@ -15,26 +15,40 @@ namespace Soil
   void printSoilMoistureSensorValues(); //Comment out/delete this function for the real program
   void updateSoilSensorValue();
 
+  /**
+   * @brief Initializes correct pins. Uses `soil_sensor_pin` and `soil_sensor_power_pin` variables as pins
+   * 
+   */
   void initiateSoilSensor() 
   {
-    pinMode(soil_sensor,INPUT);
-    pinMode(soil_sensor_power,OUTPUT);
-    digitalWrite(soil_sensor_power,LOW); 
-    Serial.begin(9600);
+    pinMode(soil_sensor_pin,INPUT);
+    pinMode(soil_sensor_power_pin,OUTPUT);
+    digitalWrite(soil_sensor_power_pin,LOW); 
+  }
+
+  void initiateSoilSensor(uint16_t soil_sensor_pin, uint16_t soil_sensor_power_pin) 
+  {
+    Soil::soil_sensor_pin = soil_sensor_pin;
+    Soil::soil_sensor_power_pin = soil_sensor_power_pin;
+
+    pinMode(soil_sensor_pin,INPUT);
+    pinMode(soil_sensor_power_pin,OUTPUT);
+    digitalWrite(soil_sensor_power_pin,LOW); 
   }
 
   void updateSoilSensorValue() 
   {
       readSoilMoistureSensor();
-      digitalWrite(soil_sensor_power,LOW);
+      digitalWrite(soil_sensor_power_pin,LOW);
       turnSoilMoistureToPrecentage(); 
       printSoilMoistureSensorValues();//Comment out/delete this function for the real program, just debugging  
   }
 
   void readSoilMoistureSensor()
   {
-    digitalWrite(soil_sensor_power,HIGH);
-    soil_reading_value = analogRead(soil_sensor);
+    digitalWrite(soil_sensor_power_pin,HIGH);
+    delay(10);
+    soil_reading_value = analogRead(soil_sensor_pin);
   }
 
   //Comment out/delete this function for the real program, just for checking that the sensor is working
