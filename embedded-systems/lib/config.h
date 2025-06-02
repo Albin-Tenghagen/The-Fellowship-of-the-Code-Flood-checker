eb476a5847588fe685cb018a7250dcbaa8d96dfd
Love Lindeborg <lindeborglove@gmail.com>
2025-06-02 11:31:19 +0200

    Moved and updated config.h

---
+++ embedded-systems/lib/config.h
@@ -0,0 +1,25 @@
+#ifndef CONFIGURATIONS_H
+#define CONFIGURATIONS_H
+
+// ---- Unit 1 ----
+
+#define SOIL_SENSOR_PIN						4
+#define SOIL_SENSOR_POWER_PIN				5
+
+#define DHT11_SENSOR_PIN					8
+
+#define HCSR04_SENSOR_TRIGGER_PIN			6
+#define HCSR04_SENSOR_ECHO_PIN				7
+
+// ---- Unit 2 ----
+
+#define WATER_SENSOR_PIN            		7
+
+// -- Water sensor --
+
+#define WATER_SENSOR_CORRECTION_VALUE       1.2
+
+#define WATER_SENSOR_VALUE_AT_0m			600
+#define WATER_SENSOR_VALUE_AT_2m			3840
+
+#endif
