79b2ff393da6e77264ae4c098af96afdf2529986
Love Lindeborg <lindeborglove@gmail.com>
2025-06-02 09:49:41 +0200

    Made test code

--- embedded-systems/src/test_main.cpp
+++ embedded-systems/src/test_main.cpp
@@ -1,33 +1,44 @@
-#include <Arduino.h>
-#include "SoilSensor.h"
-#include "hcsr04.h"
-#include <DHTSensor.h>
-#include <fellowship_wifi.h>
-#include <secrets.h>
+// #include <Arduino.h>
+// #include "SoilSensor.h"
+// #include "hcsr04.h"
+// #include <DHTSensor.h>
+// #include <fellowship_wifi.h>
+// #include <secrets.h>
 
-//Delete these variables in the real program
-unsigned long previous_reading = 0;
-unsigned long interval_reading = 10000;
-float HCSR04distance = 0.0;
+// //Delete these variables in the real program
+// unsigned long previous_reading = 0;
+// unsigned long interval_reading = 10000;
+// float HCSR04distance = 0.0;
 
-void setup() 
-{
-    Serial.begin(9600);
+// void setup() 
+// {
+//     Serial.begin(9600);
 
-    while (!Serial);
+//     while (!Serial);
 
-    // JsonDocument credentials;
+//     // JsonDocument credentials;
  
-    fellowshipWiFi::connectWiFi();
+//     fellowshipWiFi::connectWiFi();
 
 
-    String token;
-    fellowshipWiFi::sendLoginRequest({192, 168, 8, 169}, 5001, "/admins/login", BACKEND_USERNAME, BACKEND_PASSWORD, BACKEND_EMAIL, token);
+//     fellowshipWiFi::sendLoginRequest({192, 168, 8, 169}, 5001, "/admins/login", BACKEND_USERNAME, BACKEND_PASSWORD, BACKEND_EMAIL);
 
-    Serial.println(token);
-}
+//     JsonDocument json;
 
-void loop() 
-{
+//     json["station_id"] = 1;
+//     json["soil_moisture_percent"] = Soil::soil_reading_value_in_precentage;
+//     json["temperature_c"] = DHTSensor::temperature;
+//     json["humidity_percent"] = DHTSensor::humidity;
+//     json["water_level_ultrasound_cm"] = distance_us;
+//     json["water_level_pressure_cm"] = water_level_cm;
+//     json["water_level_average_cm"] = (double) (distance_us + (double) water_level_cm) / 2.0; 
 
-}
+//     fellowshipWiFi::sendRequest({192, 168, 8, 169}, 5001, "/postmonitoring", )
+
+//     // Serial.println(fellowshipWiFi::token.token);
+// }
+
+// void loop() 
+// {
+
+// }
