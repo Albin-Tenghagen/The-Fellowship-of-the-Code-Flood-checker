4cced92dbd78d8d7e329eaa8f888594bdb683c6c
atfedmis <federicamisirocchi@hotmail.com>
2025-06-12 10:44:14 +0200

    this test is not working :))

---
+++ frontend-mobile/flood-app/tests/TipInputCard.test.js
@@ -0,0 +1,19 @@
+import { render } from '@testing-library/react-native';
+import TipInputCard from '../components/TipInputCard'; 
+import { ThemeProvider } from '../themes/ThemeContext'; 
+
+describe('TipInputCard', () => {
+  it('renders title and input fields', () => {
+    const { getByText, getByPlaceholderText } = render(
+      <ThemeProvider>
+        <TipInputCard />
+      </ThemeProvider>
+    );
+
+    expect(getByText('Skicka in tips')).toBeTruthy();
+    expect(getByPlaceholderText('Ange ditt namn')).toBeTruthy();
+    expect(getByPlaceholderText('Ange plats')).toBeTruthy();
+    expect(getByPlaceholderText('Beskriv ditt tips här...')).toBeTruthy();
+    expect(getByText('Skicka in')).toBeTruthy();
+  });
+});
